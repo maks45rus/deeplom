8755564d0d32fbc88e6867b5147f1a9ba6e08dd4
maksim <maksim>
2025-01-30 14:57:54 +0500

    updateapi

--- app/src/main/java/com/example/raspisanieshgpu/api/RetrofitClient.kt
+++ app/src/main/java/com/example/raspisanieshgpu/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://shgpi.edu.ru/"
+    private const val BASE_URL = "https://shspu.ru/sch_api/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
